67315a06595fa51f700349519f3173e23ebaec3a
christianjunior05 <bchrist.junior@gmail.com>
2025-09-18 12:47:57 +0000

    add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,17 @@ import { useState, useRef } from "react";
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const ClickRef = useRef<HTMLUListElement[]>([]);
-  
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
-
   const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
-  const target = event.currentTarget.querySelector('.menu');
-  if (target) {
-    target.classList.toggle("active");
-  }
-};
+    const target = event.currentTarget.querySelector(".menu");
+    if (target) {
+      target.classList.toggle("active");
+    }
+  };
 
   return (
     <nav className=" bg-blue text-white font-lato relative z-50  ">
@@ -34,59 +33,73 @@ export default function Navbar() {
           <li className="hover:text-jauneOr transition-all cursor-pointer">
             <Link to="/"> Accueil</Link>
           </li>
-          <li onClick={handleClick} className="   hover:text-jauneOr transition-all  cursor-pointer  ">
+          <li
+            onClick={handleClick}
+            className="   hover:text-jauneOr transition-all  cursor-pointer  "
+          >
             Nos offres d'orientation
-            <ul ref={el=>{ClickRef.current[0]=el!}} className=" menu   absolute z-20 text-white   leading-10 bg-blue w-44  rounded-md  ">
-            
-               <li className="cursor-pointer pl-2    bg-blue hover:text-jauneOr transition-all  ">
-              
+            <ul
+              ref={(el) => {
+                ClickRef.current[0] = el!;
+              }}
+              className=" menu   absolute z-20 text-white   leading-10 bg-blue w-44  rounded-md  "
+            >
+              <li className="cursor-pointer pl-2    bg-blue hover:text-jauneOr transition-all  ">
                 <Link to="/repere">Repère </Link>
               </li>
-              
 
               <li className="cursor-pointer pl-2    bg-blue hover:text-jauneOr transition-all  ">
-             
                 <Link to="/exploration">Exploration</Link>
               </li>
 
               <li className="cursor-pointer pl-2    bg-blue hover:text-jauneOr transition-all   ">
                 <Link to="/trajectoire">Trajectoire </Link>
               </li>
-              
-              
+
               <li className="cursor-pointer pl-2      rounded-b-md   bg-blue hover:text-jauneOr transition-all  ">
-             
                 <Link to="/parcours">Parcoursup</Link>
               </li>
             </ul>
           </li>
-          <li onClick={handleClick} className=" hover:text-jauneOr transition-all   space-y-5  cursor-pointer ">
-             <Link to="/concours"> Nos stages</Link> 
-           
+          <li
+            onClick={handleClick}
+            className=" hover:text-jauneOr transition-all   space-y-5  cursor-pointer "
+          >
+            <Link to="/concours"> Nos stages</Link>
           </li>
           <li className="hover:text-jauneOr transition-all cursor-pointer">
-            <Link to="/conseillers">   Nos conseillers</Link>
-         </li>
-          <li onClick={handleClick} className="hover:text-jauneOr transition-all cursor-pointer">
+            <Link to="/conseillers"> Nos conseillers</Link>
+          </li>
+          <li
+            onClick={handleClick}
+            className="hover:text-jauneOr transition-all cursor-pointer"
+          >
             Qui sommes-nous
-
-              <ul  ref={el=>{ClickRef.current[1]=el!}} className="absolute menu ChildMenu text-white    bg-blue   rounded-md ">
-             
+            <ul
+              ref={(el) => {
+                ClickRef.current[1] = el!;
+              }}
+              className="absolute menu ChildMenu text-white    bg-blue   rounded-md "
+            >
               <li className="cursor-pointer hover:text-jauneOr p-2 rounded-md transition-all bg-blue ">
-               <Link to="/QuiSommesNous">Qui sommes-nous</Link>
-              </li>
-             
-              <li className="cursor-pointer hover:text-jauneOr p-2 rounded-md transition-all bg-blue ">
-               <Link to="/notreengagement">Notre engagement</Link>
+                <Link to="/QuiSommesNous">Notre Equipe</Link>
               </li>
 
-               <li className="cursor-pointer hover:text-jauneOr p-2 rounded-md transition-all bg-blue ">
-               <Link to="/devenirconseiller">Devenir conseillers</Link>
+              <li className="cursor-pointer hover:text-jauneOr p-2 rounded-md transition-all bg-blue ">
+                <Link to="/notreengagement">Notre engagement</Link>
+              </li>
+
+              <li className="cursor-pointer hover:text-jauneOr p-2 rounded-md transition-all bg-blue ">
+                <Link to="/devenirconseiller">Devenir conseillers</Link>
               </li>
             </ul>
-            </li>
-          <li className="hover:text-jauneOr transition-all cursor-pointer">Nous contacter</li>
-          <li className="hover:text-jauneOr transition-all cursor-pointer">Blog</li>
+          </li>
+          <li className="hover:text-jauneOr transition-all cursor-pointer">
+            Nous contacter
+          </li>
+          <li className="hover:text-jauneOr transition-all cursor-pointer">
+            Blog
+          </li>
           <li className="flex items-center space-x-2 text-jauneOr">
             <HiOutlineLockClosed />
             <span>Connexion</span>
