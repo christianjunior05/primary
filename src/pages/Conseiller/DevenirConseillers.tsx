9467091ebb7b64677675a9fdc699b783e3425f69
christianjunior05 <bchrist.junior@gmail.com>
2025-09-23 00:25:55 +0000

    dd

--- src/pages/Conseiller/DevenirConseillers.tsx
+++ src/pages/Conseiller/DevenirConseillers.tsx
@@ -1,6 +1,7 @@
 import { GiCheckMark } from "react-icons/gi";
 import { HiArrowRight } from "react-icons/hi2";
 import { IoIosArrowDown } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 
 export default function DevenirConseillers() {
@@ -229,7 +230,40 @@ export default function DevenirConseillers() {
             </div>
           </div>
 </section>
+
+ <div className="     flex items-center justify-center gap-5  max-sm:space-x-0 my-20 ">
+        <div className=" size-96  rounded-xl">
+          <img
+            src="/section6.jpg"
+            alt="image"
+            className="size-full object-cover rounded-xl "
+          />
+        </div>
+        <div className="  w-6/12  space-y-5 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center flex flex-col ">
+          <h2 className="text-blue font-lato font-dmserif    text-4xl max-md:text-center  ">
+            Vous  <span className="text-jauneOr"> pouvez aussi </span>
+          </h2>
+
+          <div  className=" items-center space-x-6 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:space-x-0 max-sm:space-y-6 flex-wrap space-y-3  ">
+            <Link to="/QuiSommesNous" className=" inline-block max-lg:text-xs text-white text-sm space-x-2   bg-jauneOr py-3 px-5  font-montserrat font-semibold  rounded-lg hover:bg-jauneOr/80      ">
+             <span>Découvrir notre équipe</span> 
+              <HiArrowRight className="font-semibold text-lg  inline " />
+            </Link>
+            <Link to="/notreengagement" className=" inline-block max-lg:text-xs text-white text-sm space-x-2   bg-jauneOr py-3 px-5  font-montserrat font-semibold  rounded-lg hover:bg-jauneOr/80      ">
+             <span>Découvrir notre engagement</span> 
+              <HiArrowRight className="font-semibold text-lg  inline " />
+            </Link>
+
+              <Link to="/parcours" className=" inline-block max-lg:text-xs text-white text-sm space-x-2   bg-jauneOr py-3 px-5  font-montserrat font-semibold  rounded-lg hover:bg-jauneOr/80      ">
+            <span> Pack parcoursup </span>   
+              <HiArrowRight className="font-semibold text-lg  inline " />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
+
+    
   );
 }
 
