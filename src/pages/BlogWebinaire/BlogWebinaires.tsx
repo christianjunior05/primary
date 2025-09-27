8fa149da740e747345de6bf3f3cc95afc463eea1
wisley225 <lewisguei67@gmail.com>
2025-09-27 10:49:54 +0000

    ajoute du blog

--- src/pages/BlogWebinaire/BlogWebinaires.tsx
+++ src/pages/BlogWebinaire/BlogWebinaires.tsx
@@ -1,5 +1,5 @@
 import { FaArrowRight } from "react-icons/fa";
-import Blog from "./Blog";
+
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
